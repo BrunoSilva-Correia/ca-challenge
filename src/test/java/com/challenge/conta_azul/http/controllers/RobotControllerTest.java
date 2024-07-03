6042021e320b84b5dd1479ed9d31466add445404
BrunoSilva-Correia <brunocorreiasf@gmail.com>
2024-07-03 15:21:01 -0300

    style: remove commented code

--- src/test/java/com/challenge/conta_azul/http/controllers/RobotControllerTest.java
+++ src/test/java/com/challenge/conta_azul/http/controllers/RobotControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -17,7 +16,6 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
-// @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RobotControllerTest {
